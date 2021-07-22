038112fcf0416db3bbda323d687621f85aa9e53e
João Fernandes <joaofnds@joaofnds.com>
2021-07-22 00:45:45 -0300

    chore: bump emacs requirement to 24.4

--- emacs-tmux-runner.el
+++ emacs-tmux-runner.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: Symbol’s value as variable is void: finder-known-keywords
 ;; Homepage: https://github.com/joaofnds/emacs-tmux-runner
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.4"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
